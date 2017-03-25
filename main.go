2356dfb784af3facbe1e3a28a712fb16612ccc95
Florian Engel <florian.engel@adletec.de>
2017-03-25 01:05:06 +0100

    Fix xor expression.

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 		}
 	}
 	flag.Parse()
-	if (*quiet ^ *verbose ^ *pidFormat) && (*quiet && *verbose && *pidFormat) {
+	if (*quiet != *verbose != *pidFormat) != (*quiet && *verbose && *pidFormat) {
 		log.Fatalln("ERR: The parameters are mutual exclusive.")
 	}
 	if *quiet || *pidFormat {
