bd45f0b36a592cbb865e3d429585f124cc474a27
Florian Engel <florian.engel@adletec.de>
2017-03-26 23:48:38 +0200

    Add ubuntu support, fix directory permissions.

--- package.sh
+++ package.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+VERSION="1.1"
+
 SOURCEDIR=$(pwd)
 TEMPDIR=$(mktemp -d)
 GO_TEMPPATH=$(mktemp -d)
@@ -11,14 +13,16 @@ GO_TEMPPATH=$(mktemp -d)
 BIN_DIR=$TEMPDIR/usr/bin
 
 mkdir -p $BIN_DIR
+chmod -R 755 $TEMPDIR/usr
 
 # copy files to target layout
 cp -r $SOURCEDIR/main $BIN_DIR/schroot-process-check
 chmod 6711 $BIN_DIR/schroot-process-check
 
 # build packages
-fpm -s dir -t rpm -n schroot-process-check --after-install set-permission.sh -C $TEMPDIR .
-fpm -s dir -t deb -n schroot-process-check --after-install set-permission.sh -C $TEMPDIR .
+fpm -s dir -t rpm -n schroot-process-check -v $VERSION --after-install set-permission.sh -C $TEMPDIR .
+fpm -s dir -t deb -n schroot-process-check -v $VERSION --after-install set-permission.sh -C $TEMPDIR .
+fpm -s dir -t pacman -n schroot-process-check -v $VERSION --after-install set-permission.sh -C $TEMPDIR .
 
 rm -rf $TEMPDIR
 rm -rf $GOPATH
